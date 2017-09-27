df242e706b0e077c969205b486b508ee9f98e711
Sondre Søberg <sonsobe@gmail.com>
2017-09-27 13:30:33 +0200

    Initial commit. Adds a simple example react app, as well as some webpack bundling stuff

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="test">Heisann, sveisann!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
