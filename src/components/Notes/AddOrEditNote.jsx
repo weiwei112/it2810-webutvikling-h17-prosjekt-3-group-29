63c57e2c2583ee8bae2c5c6624729dc1a0823657
Sondre Søberg <sonsobe@gmail.com>
2017-10-16 13:59:31 +0200

    Adds editing functionality for Notes

---
+++ src/components/Notes/AddOrEditNote.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {PropTypes} from 'prop-types';
+import Note from './Note.jsx';
+import TextArea from './TextArea.jsx';
+
+export default class AddOrEditNote extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {btn: this.props.tempNote};
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleClicked = this.handleClicked.bind(this);
+    this.handleUnclicked = this.handleUnclicked.bind(this);
+  }
+
+  handleButtonClick(btnState) {
+    this.setState({btn: btnState});
+  }
+
+  handleClicked() {
+    this.handleButtonClick(false);
+  }
+
+  handleUnclicked() {
+    this.handleButtonClick(true);
+  }
+
+  render() {
+    const {index, addNote, edit, resetEdit} = this.props;
+    return this.state.btn || edit ?
+      <div className='content-container'>
+        <button className="clicked" onClick={this.handleClicked}>Add Note</button>
+        <TextArea
+          handleButtonClick={this.handleButtonClick}
+          index={index}
+          addNote={addNote}
+          edit={edit}
+          resetEdit={resetEdit}
+        />
+      </div>
+    :
+      <div className='content-container'>
+        <button className="unclicked" onClick={this.handleUnclicked}>Add Note</button>
+      </div>;
+  };
+}
+
+AddOrEditNote.PropTypes = {
+  index: PropTypes.int,
+  addNote: PropTypes.func,
+  resetEdit: PropTypes.func,
+  tempNote: PropTypes.bool,
+  edit: PropTypes.bool
+};
